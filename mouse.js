543e65dc9f7eea08d6202194c9a4ff01bb6f0e9f
Andreas Grech <agnt666@gmail.com>
2011-03-06 22:18:07 +0100

    added a mouse function with a move and drag handler

---
+++ mouse.js
@@ -0,0 +1,39 @@
+var mouse = function () {
+	var position, moveAction, dragAction, isLeftClicked = false;;
+	document.onmousemove = function (e) {
+		position = {x: e.clientX, y: e.clientY};
+		if (dragAction) {
+			if (isLeftClicked) {
+				dragAction(position);
+			}
+
+		}
+		if (moveAction) {
+			moveAction(position);
+		}
+	};
+
+	document.onmousedown = function (e) {
+		isLeftClicked = true;
+		return false;
+	};
+
+	document.onmouseup = function (e) {
+		isLeftClicked = false;
+		return false;
+	};
+
+	return {
+position: position,
+		  isLeftClicked: function () { return isLeftClicked; },
+		  move: function (action) {
+			  moveAction = action;
+		  },
+drag: function (action) {
+	      dragAction = action;
+      }
+
+	};
+
+};
+
