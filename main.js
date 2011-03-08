1017c2c1c2a1f26a32b6ab420b3a2d07f7723ddd
Andreas Grech <agnt666@gmail.com>
2011-03-08 09:24:26 +0100

    started work on a vector2 function

--- main.js
+++ main.js
@@ -67,7 +67,6 @@
 					return;
 				}
 				library.loadNewImage(clientImage);
-
 			};
 		});
 
---
+++ vector2.js
@@ -0,0 +1,20 @@
+var vector2 = function(x, y) {
+	var liesBetween = function(line) { // line is assumed to an array of two vectors
+		if (line.length != 2) {
+			throw "A line needs two points, no less and I certainly don't want more; just give me the edges";
+		}
+		var a = line[0],
+		b = line[2],
+		c = {
+			x: x,
+			y: y
+		};
+		var crossproduct = (c.y - a.y) * (b.x - a.x) - (c.x - a.x) * (b.y - a.y);
+
+	};
+	return {
+		x: x,
+		y: y
+	};
+};
+
