9d3d893d5ed8d7fabb205bf1b733e4d4212c27c8
Andreas Grech <agnt666@gmail.com>
2011-03-08 04:20:52 -0800

    fixed a stupid mistake about array ordinals

--- vector2.js
+++ vector2.js
@@ -1,10 +1,10 @@
 var vector2 = function(x, y) {
-	var liesBetween = function(line) { // line is assumed to an array of two vectors
+	var liesBetween = function(line) { // line is assumed to be an array of two vectors
 		if (line.length != 2) {
 			throw "A line needs two points, no less and I certainly don't want more; just give me the edges";
 		}
 		var a = line[0],
-		b = line[2],
+		b = line[1],
 		c = {
 			x: x,
 			y: y
